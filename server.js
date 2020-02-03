fe622ba76d84668f44b8d2686fbcf546c1d73ba7
mrlmaia <murilomaia.bb@gmail.com>
2020-02-02 22:10:31 -0300

    conectando ao banco de dados

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const requireDir = require('require-dir')
 
 // Iniciando o app
 const app = express()
 
 // Iniciando o mongoose
-mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true })
+mongoose.connect(
+  'mongodb://localhost:27017/nodeapi', 
+  { useNewUrlParser: true }
+)
+requireDir('./src/models')
+
+const Product = mongoose.model('Product')
 
 // Primeira rota
 app.get('/', (req, res) => {
-  res.send('hello world')
+  Product.create({
+    title: 'NodeJs ',
+    description: "Javascript rodando no back",
+    url: 'https://node.org'
+  })
+
+  return res.send('hello world')
 })
 
 app.listen(3001)
